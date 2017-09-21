75ac2df3498873d9002a69e2a20cb371a68694e8
Alvin Niamatali and Gyles Batara <alvin@>
2017-09-21 15:56:39 -0700

    begin work

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    var sentenceInput = $("input#sentence").val().toLowerCase().split(" ").join("");
+    var output = [];
+
+    alert(sentenceInput);
+
+
+
+
+
+
+
+
+    // alert(output.join(""));
+    event.preventDefault();
+  });
+});
